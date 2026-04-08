a42fb6151819d7d0dc68ed949f8fe83d34b6393a
ZACKBOY <sevang@s.sfusd.edu>
2026-04-08 13:48:02 -0700

    created munchkin player, continue the code

---
+++ pizza-interactive/munchkin_player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	process_movement() 
+	move_and_slide()
+
+
+func process_movement() -> void:
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("left", "right", "up", "down")
+
+	velocity = direction * SPEED
