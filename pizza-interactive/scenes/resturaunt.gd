b6568df33435cdc73169ef498b456da00134031a
ItsGabe415 <galopez2@s.sfusd.edu>
2026-04-24 14:50:23 -0700

    Title screen and scene trigger working

--- pizza-interactive/scenes/resturaunt.gd
+++ pizza-interactive/scenes/resturaunt.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pausemenu"):
+	if Input.is_action_just_pressed("ui_menu"):
 		print("pausing")
 		toggle_pause()
 
