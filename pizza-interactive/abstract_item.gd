ca5d10f2047735670840d2f68ece643f48121cea
eddiespaget <edsalas@s.sfusd.edu>
2026-05-01 15:01:40 -0700

    ENEMY DROPS ITEM

--- pizza-interactive/abstract_item.gd
+++ pizza-interactive/abstract_item.gd
@@ -4,8 +4,7 @@ extends Sprite2D
 @export var ID = "0"
  
 func _ready():
-	self.texture = ItemData.get_texture() #load("res://assets/curated/" + ItemData.get_texture(ID))
- 
+	self.texture = ItemData.get_texture(int(ID)) 
  
 func _on_body_entered(_body):
 	get_parent().find_child("Inventory").add_item(int(ID))
