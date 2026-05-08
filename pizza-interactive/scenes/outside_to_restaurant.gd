eee0bc518fe3fc57562cce2689b499bcc5bcf992
eddiespaget <edsalas@s.sfusd.edu>
2026-05-08 15:46:38 -0700

    remade 3 scenes for zack with pause screens and invewntory

---
+++ pizza-interactive/scenes/outside_to_restaurant.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var player_inside = null
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_inside = body
+
+func _on_body_exited(body: Node2D) -> void:
+	if body == player_inside:
+		player_inside = null
+
+func _process(_delta):
+	if player_inside and Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://scenes/real_restaurant.tscn")
