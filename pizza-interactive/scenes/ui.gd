f6f261534a7061fbbc662d51a34bdfbf84153cda
eddiespaget <edsalas@s.sfusd.edu>
2026-05-11 15:33:36 -0700

    fixed inventory visibility

--- pizza-interactive/scenes/ui.gd
+++ pizza-interactive/scenes/ui.gd
@@ -3,6 +3,10 @@ class_name RootUIControl
 ## This controls the visibility of the inventory screen.
 
 
+func _ready():
+	visible = false
+
+
 func _input(event):
 	if event.is_action_pressed("inventory"):
 		self.visible = !self.visible
