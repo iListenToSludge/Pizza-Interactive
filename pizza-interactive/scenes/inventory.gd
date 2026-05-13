f34ca07541d9141311a191095c164ec2ef5120ad
eddiespaget <edsalas@s.sfusd.edu>
2026-05-13 13:49:33 -0700

    5 mins into the vid
    dont know for sure if itll work

--- pizza-interactive/scenes/inventory.gd
+++ pizza-interactive/scenes/inventory.gd
@@ -1,21 +1,49 @@
 extends GridContainer
 class_name Inventory
 
+signal item_changed
 
-func add_item(ID : int = 0):
-	var item_texture = ItemData.get_texture(ID) #load("res://assets/curated/" + ItemData.get_texture(ID))
-	var item_slot_type = ItemData.get_slot_type(ID)
-	var item_ATK = ItemData.get_ATK(ID)
- 
-	var item_data = {"TEXTURE": item_texture,
-					 "ATK": item_ATK,
-					 "SLOT_TYPE": item_slot_type}
-	
-	var index = 0
-	# Iterating through slots
-	for i in get_children():
-		if i.filled == false:
-			# Fill the first empty slot we encounter
-			index = i.get_index()
-			break
-	get_child(index).set_property(item_data)
+#region Testing
+@export var ITEM : ItemResource
+@export var ITEM2 : ItemResource
+
+func _ready():
+	await get_tree().create_timer(2).timeout
+	add_item(ITEM)
+	await get_tree().create_timer(2).timeout
+	add_item(ITEM2)
+#endregion
+
+func add_item(item: ItemResource):
+	var item_data = {
+		"TEXTURE": item.item_texture,
+		"NAME": item.item_name,
+		"ATK": item.item_atk_power,
+		"SLOT_TYPE": item.item_slot_type
+	}
+
+	for slot in get_children():
+		if slot.filled == false:
+			slot.set_property(item_data)
+			item_changed.emit()
+			return true
+
+	return false
+
+	return false
+
+func remove_item(ID : int):
+	for slot in get_children():
+		if slot.filled and slot.item_data["ID"] == ID:
+			slot.clear_slot()
+			item_changed.emit()
+			return true
+
+	return false
+
+func is_available(ID : int):
+	for slot in get_children():
+		if slot.filled and slot.item_data["ID"] == ID:
+			return true
+
+	return false
--- pizza-interactive/scenes/slots/slot.gd
+++ pizza-interactive/scenes/slots/slot.gd
@@ -1,11 +1,22 @@
 extends PanelContainer
 class_name Slot
 
+var item_data = null
+
 @onready var texture_rect: TextureRect = $TextureRect
 @export var item_id : String = "0"
 @export var item_resource : ItemResource = null
 
 @export_enum("NONE:0","HEAD:1","BODY:2","LEG:3", "ACTIVE:4") var slot_type : int
+
+@export var item : ItemResource = null:
+	set(value):
+		item = value
+ 
+		if value != null:
+			$TextureRect.texture = value.icon
+		else:
+			$TextureRect.texture = null
 
 var filled : bool = false
 
@@ -20,9 +31,27 @@ func _can_drop_data(_pos, data):
 
 
 func _drop_data(_pos, data):
-	var temp = texture_rect.property
-	texture_rect.property = data.property
-	data.property = temp
+	var other_slot = data.get_parent()
+
+	# swap item data between slots
+	var temp = item_data
+	item_data = other_slot.item_data
+	other_slot.item_data = temp
+
+	# update visuals
+	if item_data != null:
+		texture_rect.texture = item_data["TEXTURE"]
+		filled = true
+	else:
+		texture_rect.texture = null
+		filled = false
+
+	if other_slot.item_data != null:
+		other_slot.texture_rect.texture = other_slot.item_data["TEXTURE"]
+		other_slot.filled = true
+	else:
+		other_slot.texture_rect.texture = null
+		other_slot.filled = false
 
 func get_preview():
 	var preview_texture = TextureRect.new()
@@ -42,14 +71,21 @@ func get_ATK():
 
 
 func set_property(data):
-	texture_rect.property = data
+	item_data = data
 
-	if data["TEXTURE"] == null:
-		printerr("<Slot>: Texture not defined in item data")
-		#filled = false
+	if data == null or data["TEXTURE"] == null:
+		texture_rect.texture = null
+		filled = false
+		printerr("<Slot>: Invalid item data")
 	else:
+		texture_rect.texture = data["TEXTURE"]
 		filled = true
  
 
 func set_item_resource_data(resource_data : ItemResource):
 	self.item_resource = resource_data
+
+func clear_slot():
+	item_data = null
+	texture_rect.texture = null
+	filled = false
