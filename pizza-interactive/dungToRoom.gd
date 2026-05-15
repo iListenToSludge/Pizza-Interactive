2282f9ed6eac434ded5e62a8814821da8e36a72c
ZACKBOY <sevang@s.sfusd.edu>
2026-05-14 19:13:16 -0700

    made more scenes
    with triggers

---
+++ pizza-interactive/dungToRoom.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var player_inside = null
+
+func _process(_delta: float) -> void:
+	if player_inside and Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://resturaunttoroom.tscn")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_inside = body
+
+func _on_body_exited(body: Node2D) -> void:
+	if body == player_inside:
+		player_inside = null
