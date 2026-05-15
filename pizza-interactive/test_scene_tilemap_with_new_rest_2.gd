7a7158e2be8f8678508b30337348c1de9a21ae2a
ZACKBOY <sevang@s.sfusd.edu>
2026-05-15 15:50:07 -0700

    MADE INVENTORY BUT NOT DONE YET

--- pizza-interactive/test_scene_tilemap_with_new_rest_2.gd
+++ pizza-interactive/test_scene_tilemap_with_new_rest_2.gd
@@ -5,3 +5,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+
+
+func _on_inventory_gui_closed() -> void:
+	get_tree().paused = false
+
+func _on_inventory_gui_opened() -> void:
+	get_tree().paused = true
