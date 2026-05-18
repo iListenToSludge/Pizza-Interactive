fd71951872270c56de10898cc5db0250c353429c
eddiespaget <edsalas@s.sfusd.edu>
2026-05-18 15:47:38 -0700

    make enemy drop the actual ingredients then done
    literally need like 30 mins

--- pizza-interactive/abstract_item.gd
+++ pizza-interactive/abstract_item.gd
@@ -6,6 +6,8 @@ extends Sprite2D
 func _ready():
 	self.texture = ItemData.get_texture(int(ID)) 
  
+
 func _on_body_entered(_body):
-	get_parent().find_child("Inventory").add_item(int(ID))
+	var item_resource = ItemData.get_item(int(ID))
+	get_parent().find_child("Inventory").add_item(item_resource)
 	queue_free()
