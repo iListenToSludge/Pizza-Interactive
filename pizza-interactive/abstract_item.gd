d5d0a5b04644a1fd5724a47195596c1750dfd6ad
ZACKBOY <sevang@s.sfusd.edu>
2026-05-21 21:50:31 -0700

    notes

--- pizza-interactive/abstract_item.gd
+++ pizza-interactive/abstract_item.gd
@@ -1,13 +1,13 @@
 extends Sprite2D
 
  
-@export var ID = "0"
+@export var ID = 0
  
 func _ready():
-	self.texture = ItemData.get_texture(int(ID)) 
+	self.texture = ItemData.get_texture(ID) 
  
 
 func _on_body_entered(_body):
-	var item_resource = ItemData.get_item(int(ID))
+	var item_resource = ItemData.get_item(ID)
 	get_parent().find_child("Inventory").add_item(item_resource)
 	queue_free()
